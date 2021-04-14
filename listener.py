991406eef096ee8fe04a2d343cc183cf4a1f8725
Philo Decroos <philo0508@hotmail.com>
2021-04-14 20:00:01 +0200

    Extract position in code when traversing parse tree

--- listener.py
+++ listener.py
@@ -4,3 +4,5 @@ from parsers.python3.Python3Parser import Python3Parser
 class ASTListener(Python3Listener):
     def enterFuncdef(self, ctx:Python3Parser.FuncdefContext):
         print("FUNCTION FOUND")
+        # Get position in original code:
+        print("POSITION IN CODE: {}:{}".format(ctx.DEF().getPayload().line, ctx.DEF().getPayload().column))
